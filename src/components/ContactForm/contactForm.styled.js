a9977aeac5f61baefc16e238f7ce430208d3e5aa
Nikolay Shein <qseboy1998@gmail.com>
2023-07-31 04:29:33 +0300

    compleate second task

---
+++ src/components/ContactForm/contactForm.styled.js
@@ -0,0 +1,17 @@
+const { default: styled } = require('@emotion/styled');
+
+export const ContactUiForm = styled.form`
+  width: 400px;
+`;
+export const ContactDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+  margin-bottom: 15px;
+`;
+export const ContactLabel = styled.label`
+  width: 100%;
+  font-size: 18px;
+  font-weight: 600;
+`;
+export const ContactButton = styled.button``;
---
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+export default function FilterContacts({
+  title,
+  handleFilter,
+  state: { filter },
+}) {
+  return (
+    <>
+      <h3>{title}</h3>
+      <input
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={event => handleFilter(event)}
+      />
+    </>
+  );
+}
+
+FilterContacts.propTypes = {
+  title: PropTypes.string.isRequired,
+  handleFilter: PropTypes.func.isRequired,
+  state: PropTypes.shape({ filter: PropTypes.string.isRequired }).isRequired,
+};
